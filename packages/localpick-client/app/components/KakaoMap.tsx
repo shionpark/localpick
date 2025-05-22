e5a03a59fb8c491d9772590a1c6db25f0f71b76f
Shion <seoy1108@gmail.com>
2025-05-22 17:58:59 +0900

    feat: KakaoMap 컴포넌트 생성

---
+++ packages/localpick-client/app/components/KakaoMap.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Script from "next/script";
+import { useKakaoMapLoader } from "../hooks/useKakaoMapLoader";
+import { KAKAO_API_BASE_URL } from "../constants/urls";
+
+export default function KakaoMap() {
+  const { mapContainer, onLoadKakaoAPI, onLoadFail } = useKakaoMapLoader();
+
+  const addParams = new URLSearchParams({
+    appkey: process.env.NEXT_PUBLIC_KAKAO_KEY || "",
+    autoload: "false",
+    libraries: "services",
+  }).toString();
+  const kakaoMapUrl = `${KAKAO_API_BASE_URL}?${addParams}`;
+
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        type="text/javascript"
+        src={kakaoMapUrl}
+        onLoad={onLoadKakaoAPI}
+        onError={onLoadFail}
+      />
+      <div ref={mapContainer} className="w-[60rem] h-[40rem]"></div>
+    </>
+  );
+}
---
+++ packages/localpick-client/app/constants/urls.ts
@@ -0,0 +1,1 @@
+export const KAKAO_API_BASE_URL = "//dapi.kakao.com/v2/maps/sdk.js";
