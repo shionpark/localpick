a2f987ac02d43d0871258558fc7497d6a44e72f1
Shion <seoy1108@gmail.com>
2025-05-24 11:25:15 +0900

    chore: 모노레포 환경 재구성 및 필요한 의존성 추가
    - 스토리북 문서 로드 안되는 에러 해결

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -2,10 +2,17 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(ts|tsx|js|jsx)"],
-  addons: [],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-docs",
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  docs: {
+    autodocs: true,
   },
 };
 
---
+++ packages/localpick-client/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./packages/localpick-client/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
